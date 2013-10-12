25b322fb4265102fa71ad642a17e84cf3c817ef3
Raúl Montes <raulmt@gmail.com>
2013-10-12 19:41:47 -0300

    Ver una iniciativa

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -1,3 +1,7 @@
 class Initiative < ActiveRecord::Base
   belongs_to :ong
+  has_one :initiative_stat
+  
+  delegate :signs_count, to: :initiative_stat
+  delegate :donations_count, to: :initiative_stat
 end
