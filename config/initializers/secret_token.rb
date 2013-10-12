db9265e3fa6949f722793a79663391118b492095
John Owen <jowen@redvel.cl>
2013-10-11 21:04:21 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedoo::Application.config.secret_key_base = 'f8b3454f0dc29861889edf7615869c5ae5fc2b2ae54bf1ebd04faabf74315a38d83035812e0a676cd77820ebbba850d0c35d52f7b9feff94be9e8048fd29dce6'
