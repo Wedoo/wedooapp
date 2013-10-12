fdde617739bb9d775abe0ee14c408e0c920ce16e
Raúl Montes <raulmt@gmail.com>
2013-10-12 11:30:54 -0300

    * gema para bootstrap 3 * controller home (sin configurar root_path aun)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wedoo::Application.routes.draw do
+  get "home/index"
+  get "home/welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
