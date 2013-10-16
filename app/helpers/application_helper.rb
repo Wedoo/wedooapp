6f8b899fcfd87e50bf4eede3f627ca9998b3c8fe
Raúl Montes <raulmt@gmail.com>
2013-10-16 13:26:15 -0300

    Arrelgos generales de estilos, forms y locales  * traducciones a español de devise, generales de Rails y algo de modelos  * config de simple_form para que genere los forms compatibles con el estilo  * arreglos en el form de login  * estilos de los alerts  * agregado botón de login arriba a la izq (mientras tanto... luego Rodrigo nos proveerá :P)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def alert_classes(name)
+    case name
+    when :notice
+      return 'alert-info'
+    when :alert
+      return 'alert-danger'
+    else
+      return ''
+    end
+  end
+  
 end
