859468dbe30d69dcdafc7d901b16c5daf2b0cd1f
Raúl Montes <raulmt@gmail.com>
2013-10-16 16:58:43 -0300

    Agregado link para compartir iniciativa en FB

--- app/helpers/initiatives_helper.rb
+++ app/helpers/initiatives_helper.rb
@@ -4,4 +4,8 @@ module InitiativesHelper
     "https://twitter.com/intent/tweet?text=#{initiative.title}&hashtags=#{initiative.hashtag}&url=#{ong_initiative_url(ong, initiative)}&via=wedoo"
   end
   
+  def facebook_url(initiative)
+    "https://www.facebook.com/sharer/sharer.php?u=#{ong_initiative_url(ong, initiative)}"
+  end
+  
 end
